fac48f024184fd7b0d3f026006534649bae470a2
Tanujeet <singhtanujeet@gmail.com>
2026-03-12 21:14:24 +0530

    Fix: PACKAGE.JSON

--- apps/api-service/src/index.ts
+++ apps/api-service/src/index.ts
@@ -5,6 +5,10 @@ import dashboardRoutes from "./routes/dashboard.routes";
 import jobRoutes from "./routes/jobs";
 import workersRoutes from "./routes/workers.routes";
 
+
+console.log("SERVER BOOTING...");
+console.log("PORT:", process.env.PORT);
+
 dotenv.config();
 
 const app = express();
